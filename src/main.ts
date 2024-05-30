3f557ec4835915289bc2419dd6229ce5ab2a9f43
BOTYAI\mengh <65160309@go.buu.ac.th>
2024-05-30 22:04:13 +0700

    Add vuetify app.use

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { createVuetify } from 'vuetify'
+import { md1 } from 'vuetify/blueprints'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  blueprint: md1,
+  icons: {
+    defaultSet: 'mdi' // This is already the default value - only for display purposes
+  },
+  components,
+  directives
+})
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(vuetify)
 app.mount('#app')
