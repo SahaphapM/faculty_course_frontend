82c61130093e0403364eb5dbbeb64315ad84b1fb
chanon thansawad <65160066@go.buu.ac.th>
2024-05-31 20:50:27 +0700

    IFview

--- src/router/index.ts
+++ src/router/index.ts
@@ -20,7 +20,16 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/mainIFCurriculumView',
+      name: 'mainIFCurriculumView',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/IFCurriculum/MainIFCurriculumView.vue')
     }
+    
   ]
 })
 function isLogin() {
