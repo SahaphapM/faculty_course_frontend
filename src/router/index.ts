7d0aff7fb0df38bed6c43970cc858917950931f2
BOTYAI\mengh <65160309@go.buu.ac.th>
2024-07-08 19:44:40 +0700

    TestorView

--- src/router/index.ts
+++ src/router/index.ts
@@ -67,6 +67,17 @@ const router = createRouter({
         layout: 'MainLayout',
         requireAuth: true
       }
+    },
+    {
+      path: '/test',
+      name: 'test',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/users/UserView.vue'),
+      meta: {
+        layout: 'MainLayout'
+      }
     }
   ]
 })
