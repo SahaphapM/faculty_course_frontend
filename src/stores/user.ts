392fdfc14cf3e8111c6341ac137b0a855ee663c0
BOTYAI\mengh <65160309@go.buu.ac.th>
2024-07-16 17:24:00 +0700

    Paging, Searching with v-data table server

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -2,6 +2,7 @@ import { ref } from 'vue'
 import { defineStore } from 'pinia'
 import type { User } from '@/types/User'
 import userService from '@/service/user'
+import type { PageParams } from '@/types/PageParams'
 
 export const useUserStore = defineStore('user', () => {
   const users = ref<User[]>([])
@@ -31,8 +32,8 @@ export const useUserStore = defineStore('user', () => {
     users.value.push(user)
   }
 
-  async function fetchUsersPage(page: number, limit: number) {
-    const res = await userService.getUsersByPage(page, limit)
+  async function fetchUsersPage(params: PageParams) {
+    const res = await userService.getUsersByPage(params)
     users.value = res.data.data
     totalUsers.value = res.data.total
   }
--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,5 +1,82 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref, watch } from 'vue'
+import axios from 'axios'
+import { useUserStore } from '@/stores/user'
+import type { User } from '@/types/User'
+import type { PageParams } from '@/types/PageParams'
+
+const userStore = useUserStore()
+
+const headers = [
+  { title: 'Email', value: 'email' },
+  { title: 'First Name', value: 'firstName' },
+  { title: 'Last Name', value: 'lastName' },
+  { title: 'Gender', value: 'gender' },
+  { title: 'Phone', value: 'phone' },
+  { title: 'Roles', value: 'roles' },
+  { title: 'Actions', value: 'actions', sortable: false }
+]
+
+const loading = ref(false)
+const pageParams = ref<PageParams>({
+  page: 1,
+  limit: 10,
+  sort: '',
+  order: 'ASC',
+  search: ''
+})
+
+const search = ref('')
+
+const fetchUsers = async () => {
+  loading.value = true
+  try {
+    await userStore.fetchUsersPage(pageParams.value)
+  } catch (error) {
+    console.error('Error fetching users:', error)
+  } finally {
+    loading.value = false
+  }
+}
+const updateOptions = (options: any) => {
+  pageParams.value.page = options.page
+  pageParams.value.limit = options.itemsPerPage
+}
+
+watch(pageParams, fetchUsers, { deep: true })
+watch(search, fetchUsers)
+
+onMounted(async () => {
+  await fetchUsers()
+  console.log(userStore.totalUsers)
+})
+</script>
 
 <template>
-  <main>home</main>
+  <div>
+    <v-data-table-server
+      v-model:items-per-page="pageParams.limit"
+      :headers="headers"
+      :items="userStore.users"
+      :items-length="userStore.totalUsers"
+      :loading="loading"
+      item-value="name"
+      @update:options="updateOptions"
+    >
+      <template v-slot:item.roles="{ item }">
+        <v-chip-group>
+          <v-chip v-for="role in item.roles" :key="role.id">
+            {{ role.name }}
+          </v-chip>
+        </v-chip-group>
+      </template>
+    </v-data-table-server>
+  </div>
 </template>
+
+<style scoped>
+.mx-4 {
+  margin-left: 16px;
+  margin-right: 16px;
+}
+</style>
