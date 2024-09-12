7f0e4b3914cee6a97efbd80437180a0f89fca44d
Aphidech Mahakoat <jjack46.flowsick@gmail.com>
2024-09-12 21:59:59 +0700

    prototype student drawer

---
+++ src/views/student/Student_Scores.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>scores</div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
---
+++ src/views/student/Student_Skills.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>skills</div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
